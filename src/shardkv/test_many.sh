319930838cfbd669d13f20c2f6101c8fd9a9a82e
gjz010 <gjz010944@gmail.com>
2021-06-22 20:26:23 +0800

    test_many fix

--- src/shardkv/test_many.sh
+++ src/shardkv/test_many.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 counter=0
-while time go test -failfast -run TestStaticShards -race > shardkv.log ; do echo Test \#$counter success.; counter=$(($counter+1)); done
+while time go test -failfast -run "TestStaticShards|TestJoinLeave|TestSnapshot|TestMissChange|TestConcurrent|TestUnreliable" -race > shardkv.log ; do echo Test \#$counter success.; counter=$(($counter+1)); done
 echo Fail at test $counter
