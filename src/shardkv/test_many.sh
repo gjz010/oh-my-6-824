a3276b7b71b50016ad9699241e65cebe95a6286b
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 17:27:15 +0000

    log disabled

--- src/shardkv/test_many.sh
+++ src/shardkv/test_many.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-LOG_NAME=${$1:shardkv.log}
 counter=0
-while time go test -failfast -run "TestStaticShards|TestJoinLeave|TestSnapshot|TestMissChange|TestConcurrent|TestUnreliable" -race > $LOG_NAME ; do echo Test \#$counter success.; counter=$(($counter+1)); done
+while time go test -failfast -run "TestStaticShards|TestJoinLeave|TestSnapshot|TestMissChange|TestConcurrent|TestUnreliable" ; do echo Test \#$counter success.; counter=$(($counter+1)); done
 echo Fail at test $counter
