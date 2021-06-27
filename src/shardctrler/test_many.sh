25e49000b00ee2ed2024b5bb935bf9f025511315
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 17:11:53 +0000

    final commit

--- src/shardctrler/test_many.sh
+++ src/shardctrler/test_many.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-LOG_NAME=${$1:shardctrler.log}
+LOG_NAME=$1
+LOG_NAME=${LOG_NAME:=shardctrler.log}
 counter=0
 while time go test -failfast -race > $LOG_NAME ; do echo Test \#$counter success.; counter=$(($counter+1)); done
 echo Fail at test $counter
--- src/shardkv/test_unreliable_2.sh
+++ src/shardkv/test_unreliable_2.sh
@@ -2,6 +2,6 @@
 LOG_NAME=$1
 LOG_NAME=${LOG_NAME:="shardkv.log"}
 counter=0
-while time go test  -timeout 1200s -failfast -run "TestStaticShards|TestJoinLeave|TestSnapshot|TestMissChange|TestConcurrent|TestUnreliable"  > $LOG_NAME ; do echo Test \#$counter success.; counter=$(($counter+1)); done
+while time go test  -timeout 1200s -failfast -run "TestUnreliable2"  > $LOG_NAME ; do echo Test \#$counter success.; counter=$(($counter+1)); done
 #while time go test -failfast -run "TestSnapshot" -race > $LOG_NAME ; do echo Test \#$counter success.; counter=$(($counter+1)); done
 echo Fail at test $counter
